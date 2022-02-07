2eac9cf72e0437a796e7de4f6f36647607e770ed
Charity King <csking@andrew.cmu.edu>
2022-02-07 11:50:55 -0500

    Initial submission for BiLSTM model, require error rate analysis

---
+++ BiLSTM/data_analysis.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  5 00:37:16 2022
+
+@author: csking
+"""
+
+import os, sys
+import argparse
+
+
+
+def main()
+
+if __name__ == "__main__":
+    main()
